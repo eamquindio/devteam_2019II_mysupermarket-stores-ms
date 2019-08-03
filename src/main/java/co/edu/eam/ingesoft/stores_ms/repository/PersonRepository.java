1786804a8f9b45f229bfa3fb64fc090e2572f817
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-08-03 17:09:18 -0500

    example

--- src/main/java/co/edu/eam/ingesoft/stores_ms/repository/PersonRepository.java
+++ src/main/java/co/edu/eam/ingesoft/stores_ms/repository/PersonRepository.java
@@ -1,5 +1,7 @@
 package co.edu.eam.ingesoft.stores_ms.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import co.edu.eam.ingesoft.stores_ms.model.Person;
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Integer>{
 
+	List<Person> findAll();
+	
+	List<Person> findByName(String name);
 }
