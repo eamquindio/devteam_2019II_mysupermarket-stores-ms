dd55e6d4862d4d6f7aae3a55e82b3b0ae725a827
HERNAN MAURICIO VELEZ ROJAS <hernanmauriciovelezrojas@HERNAN-MacBook-Pro.local>
2019-08-10 11:57:39 -0500

    [resolves#8]list_store_by_name

--- src/main/java/co/edu/eam/ingesoft/stores_ms/controllers/StoresController.java
+++ src/main/java/co/edu/eam/ingesoft/stores_ms/controllers/StoresController.java
@@ -1,24 +1,28 @@
 package co.edu.eam.ingesoft.stores_ms.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import co.edu.eam.ingesoft.stores_ms.model.Stores;
 import co.edu.eam.ingesoft.stores_ms.services.StoreService;
 import co.edu.eam.ingesoft.stores_ms.services.PersonService;
+import co.edu.eam.ingesoft.stores_ms.model.Person;
+import co.edu.eam.ingesoft.stores_ms.model.Stores;
 import co.edu.eam.ingesoft.stores_ms.services.StoresService;
 
 @RestController
 @RequestMapping("api/stores-ms/stores")
 
 public class StoresController {
+	
+
 
 	@Autowired
 	private StoreService storeService;
@@ -36,4 +40,17 @@ public class StoresController {
 	public void delete(@PathVariable String id) {
 		storeService.delete(id);
 	}
+
+	/*
+	 * method to list store names
+	 */
+	@GetMapping(value="/find_by_name")
+	public List<Stores> findByName(@RequestParam String name) {
+    	System.out.println(name);
+    	return storeService.findByName(name);
+    	}
+    
+  
 }
+
+
--- src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoreService.java
+++ src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoreService.java
@@ -1,7 +1,11 @@
 package co.edu.eam.ingesoft.stores_ms.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import co.edu.eam.ingesoft.stores_ms.model.Person;
 import co.edu.eam.ingesoft.stores_ms.model.Stores;
 import co.edu.eam.ingesoft.stores_ms.repository.StoresRepository;
 
@@ -18,5 +22,9 @@ public class StoreService {
 	// interface call the method
 	public void delete(String id) {
 		storeRepository.deleteById(id);
+	}
+	
+	public List <Stores> findByName(String name) {
+		return storeRepository.findByName(name);
 	}
 }
