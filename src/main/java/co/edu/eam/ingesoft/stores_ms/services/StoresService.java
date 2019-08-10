9efcf017aa4e519af3368d3786d1b050172e9e0e
m9n0lo <xthernux@gmail.com>
2019-08-10 11:20:26 -0500

    [resolves #11] delete stores

--- src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoresService.java
+++ src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoresService.java
@@ -3,7 +3,7 @@ package co.edu.eam.ingesoft.stores_ms.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import co.edu.eam.ingesoft.stores_ms.repository.StoreRepository;
+
 import co.edu.eam.ingesoft.stores_ms.repository.PersonRepository;
 
 @Service
