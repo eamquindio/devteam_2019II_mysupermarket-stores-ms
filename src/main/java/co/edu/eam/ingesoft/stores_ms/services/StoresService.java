299c892a7b9474bca5208eba9f3ccedae33a4e22
Mauricio Ruiz <2320151015@eam.edu.co>
2019-08-10 10:43:18 -0500

    [resolver #13] create stores

--- src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoresService.java
+++ src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoresService.java
@@ -3,14 +3,16 @@ package co.edu.eam.ingesoft.stores_ms.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.eam.ingesoft.stores_ms.model.Stores;
 import co.edu.eam.ingesoft.stores_ms.repository.StoresRepository;
-
 @Service
 public class StoresService {
 	
 	@Autowired
 	private StoresRepository storesRepository;
 	
-	
+	public void create(Stores stores) {
+		storesRepository.save(stores);
+	}
 
 }
