d79396f45a06b1135d7d0b5da2f2130b1d35b19f
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-08-16 07:10:30 -0300

    testing checkstyle

---
+++ src/main/java/test/Calculator.java
@@ -0,0 +1,22 @@
+package test;
+
+/**
+ * Class to perform math operations.
+ *
+ * @author caferrerb
+ *
+ */
+public class Calculator {
+
+  /**
+   * Perform a sum.
+   *
+   * @param a firt operand.
+   * @param b second operand.
+   * @return integer with a plus b
+   */
+  public int sumar(int a, int b) {
+    return a + b;
+  }
+
+}
