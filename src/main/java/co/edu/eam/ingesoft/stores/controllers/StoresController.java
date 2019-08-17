2475d0f2378569487fc486cb625f5a4eb8607297
conrado94 <48019268+conrado94@users.noreply.github.com>
2019-08-17 17:22:35 -0500

    [resolves #9]stores_list

--- src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
+++ src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
@@ -1,13 +1,19 @@
 package co.edu.eam.ingesoft.stores.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.eam.ingesoft.stores.model.Person;
+import co.edu.eam.ingesoft.stores.model.Stores;
 import co.edu.eam.ingesoft.stores.services.StoresService;
 
 /**
  * Stores controller.
+ * 
  * @author caferrerb
  *
  */
@@ -15,10 +21,20 @@ import co.edu.eam.ingesoft.stores.services.StoresService;
 @RequestMapping("api/stores-ms/stores")
 public class StoresController {
 
-  /**
-   * stores service.
-   */
-  @Autowired
-  private StoresService storesService;
+	/**
+	 * stores service.
+	 */
+	@Autowired
+	private StoresService storesService;
+
+	/**
+	 * list all stores.
+	 *
+	 * @return list of all stores
+	 */
+	@GetMapping(value = "/all")
+	public List<Stores> findAll() {
+		return storesService.listAll();
+	}
 
 }
--- src/main/java/co/edu/eam/ingesoft/stores/services/StoresService.java
+++ src/main/java/co/edu/eam/ingesoft/stores/services/StoresService.java
@@ -1,8 +1,12 @@
 package co.edu.eam.ingesoft.stores.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.eam.ingesoft.stores.model.Person;
+import co.edu.eam.ingesoft.stores.model.Stores;
 import co.edu.eam.ingesoft.stores.repositories.StoresRepository;
 
 /**
@@ -14,10 +18,18 @@ import co.edu.eam.ingesoft.stores.repositories.StoresRepository;
 @Service
 public class StoresService {
 
-  /**
-   * product repository.
-   */
-  @Autowired
-  private StoresRepository storesRepository;
+	/**
+	 * product repository.
+	 */
+	@Autowired
+	private StoresRepository storesRepository;
 
+	/**
+	 * List all stores.
+	 *
+	 * @return list of all stores
+	 */
+	public List<Stores> listAll() {
+		return storesRepository.findAll();
+	}
 }
