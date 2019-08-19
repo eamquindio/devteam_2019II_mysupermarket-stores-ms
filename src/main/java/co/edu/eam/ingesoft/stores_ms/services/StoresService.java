387a5292509c9796e067ca42c632d6ec07108f48
HERNAN MAURICIO VELEZ ROJAS <hernanmauriciovelezrojas@HERNAN-MacBook-Pro.local>
2019-08-19 18:13:40 -0500

    testing checkstyle

--- src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoresService.java
+++ src/main/java/co/edu/eam/ingesoft/stores_ms/services/StoresService.java
@@ -5,28 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
+
 
 import co.edu.eam.ingesoft.stores_ms.repository.PersonRepository;
 
 @Service
 public class StoresService {
-=======
-import co.edu.eam.ingesoft.stores_ms.model.Stores;
-import co.edu.eam.ingesoft.stores_ms.repository.StoreRepository;
 
-@Service
-public class StoresService {
+
 	
-	@Autowired
-	private StoreRepository storesRepository;
-
-	public static List<Stores> findByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
->>>>>>> [resolves#8]list_store_by_name
-
 
 }
