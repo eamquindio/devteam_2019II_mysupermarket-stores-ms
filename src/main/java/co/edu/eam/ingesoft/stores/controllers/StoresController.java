eb52cfe985e563e7a5e93a56dddb5e32d2552f49
m9n0lo <xthernux@gmail.com>
2019-08-20 14:15:45 -0500

    [delete_styles_stores]

--- src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
+++ src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import co.edu.eam.ingesoft.stores.model.Stores;
 import co.edu.eam.ingesoft.stores.services.StoresService;
 
 /**
  * Stores controller.
- *
+ * 
  * @author caferrerb
  *
  */
@@ -27,32 +28,40 @@ public class StoresController {
 
 
   /**
-   * find a store.
-   *
-   * @param id id for store to find
-   * @return store with id
-   */
+  * @param id id for store to find
+  * @return store with id
+  */
   @GetMapping(value = "/{id}")
   public Stores find(@PathVariable String id) {
     return storesService.find(id);
   }
 
   /**
-   * Edit a store.
-   *
-   * @param store store to edit
-   */
+  * Edit a store.
+  *
+  * @param store store to edit
+  */
   @PutMapping(value = "/")
   public void edit(@RequestBody Stores store) {
     storesService.update(store);
   }
   /**
-   * list all stores.
-   *
-   * @return list of all stores
-   */
+  * list all stores.
+  *
+  * @return list of all stores
+  */
   @GetMapping(value = "/all")
   public List<Stores> findAll() {
     return storesService.listAll();
   }
+  /**
+  * Delete a person.
+  *
+  * @param id person to delete
+  */
+  @DeleteMapping(value = "/{id}")
+  public void delete(@PathVariable String id) {
+    storesService.delete(id);
+  }
+
 }
