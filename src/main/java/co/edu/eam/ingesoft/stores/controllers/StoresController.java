d12387e46e2b4908a60b193fb326625f7358acb0
Gabriela <gabrielaaa1413@gmail.com>
2019-08-24 11:39:55 -0500

    test_update

--- src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
+++ src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import co.edu.eam.ingesoft.stores.model.Stores;
 import co.edu.eam.ingesoft.stores.routes.Router;
 import co.edu.eam.ingesoft.stores.services.StoresService;
