55d3f142b136c268e9f1bd36a5fb667a428a9f00
GabrielaMachado <gabimachadop@hotmail.com>
2019-08-24 09:03:44 -0500

    [resolves] test_find stores

--- src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
+++ src/main/java/co/edu/eam/ingesoft/stores/controllers/StoresController.java
@@ -3,9 +3,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/co/edu/eam/ingesoft/stores/services/StoresService.java
+++ src/main/java/co/edu/eam/ingesoft/stores/services/StoresService.java
@@ -1,8 +1,6 @@
 package co.edu.eam.ingesoft.stores.services;
 
 import java.util.List;
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import co.edu.eam.ingesoft.stores.model.Stores;
